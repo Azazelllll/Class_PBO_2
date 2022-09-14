ed24a980446cce91194a03902bce440464eb5b55
USER <USER@172.16.37.35>
2022-09-14 08:53:26 +0700

    Upload New Project

---
+++ src/pbo_2/PBO_2.java
@@ -0,0 +1,16 @@
+package pbo_2;
+
+/**
+ *
+ * @author USER
+ */
+public class PBO_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
